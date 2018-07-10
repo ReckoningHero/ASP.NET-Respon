f4083f2a135889358c485cfa8f3ff0c8c3eccd1e
TwinB0rn <twinb0rn@github.com>
2018-07-10 00:10:54 -0500

    Updated

---
+++ Backup .NET Files/Vidley/Vidley/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidley.Models;
+
+namespace Vidley.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Blade!" };
+
+            return View(movie);
+        }
+    }
+}
